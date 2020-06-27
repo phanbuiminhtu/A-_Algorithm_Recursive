193a994852184d12384e674cd4e5a8a2ee14cd6e
tuphan <phanbuiminhtu@gmail.com>
2020-06-26 20:38:19 -0400

    finding goal complete. Next step is to find path by adding previous block

--- venv/A*_recusive.py
+++ venv/A*_recusive.py
@@ -29,16 +29,12 @@ class Square:
     def getdistance(self):
         return self.distance
 
-
-main_list = []
-
-
-def find_path(x_ori, y_ori, x_goal, y_goal, obstacle):
-    start = time.time()
+record_list = []
+def add_block(x_ori, y_ori, x_goal, y_goal, obstacle,main_list):
     tempo_list = []
     shortest_dis = max(abs(x_ori - x_goal), abs(y_ori - y_goal))
 
-    block22 = Square(shortest_dis, x_ori, y_ori, check=True)
+    block22 = Square(shortest_dis, x_ori, y_ori, check=True)  # current block
     print("block22:", block22.x, block22.y)
 
     block11 = Square(max(abs((x_ori - 1) - x_goal), abs((y_ori + 1) - y_goal)), x_ori - 1, y_ori + 1)
@@ -55,60 +51,83 @@ def find_path(x_ori, y_ori, x_goal, y_goal, obstacle):
     tempo_list.extend([block33, block32, block11, block31, block23, block21, block12, block13])
     tempo_list = [elem for elem in tempo_list if elem.x >= 0 and elem.y >= 0]  # remove negative coordinate
 
-    print("before remove:", len(tempo_list))
+    print("before remove:", len(main_list))
 
-    for item in main_list:
+    for item in record_list:
         for elem in tempo_list:
             if elem.x == item.x and elem.y == item.y:
                 tempo_list.remove(elem)
-    for item in main_list:
+    for item in record_list:
         for elem in tempo_list:
             if elem.x == item.x and elem.y == item.y:
                 tempo_list.remove(elem)
-    print("after remove:", len(tempo_list))
-    print("")
-    main_list.append(block22)
-    # check obstacle
-    for ele in tempo_list:
-        if ele.x == x_goal and ele.y == y_goal:
-            return 1
-        for obs in obstacle:
-            if ele.x == obs.x and ele.y == obs.y:
-                tempo_list.remove(ele)
-    for ele in tempo_list:
-        if ele.x == x_goal and ele.y == y_goal:
-            return 1
-        for obs in obstacle:
-            if ele.x == obs.x and ele.y == obs.y:
-                tempo_list.remove(ele)
-    # print item in tempo_list
-  #  for item in tempo_list:
-   #     print("debug in tempo:", item.x, item.y)
 
+
+    # check obstacle to remove from tempo list
+    for ele in tempo_list:
+        for obs in obstacle:
+            if ele.x == obs.x and ele.y == obs.y:
+                tempo_list.remove(ele)
     for elem in tempo_list:
-        if elem.getdistance() < shortest_dis:
+        for obs in obstacle:
+            if elem.x == obs.x and elem.y == obs.y:
+                tempo_list.remove(elem)
+
+    # add element closer to goal to main list and "next list" of block22
+    for elem in tempo_list:
+        if elem.getdistance() < shortest_dis or elem.getdistance()==1:
             block22.setnext(elem)
+            main_list.append(elem)
+            record_list.append(elem)
+    for elem in block22.next:
+        print("block in block22.next:",elem.x,elem.y)
+    # tell the function to go along the obstacle
     if not block22.next:
         for elem in tempo_list:
-            if elem.distance == block22.distance and elem.check == False:
+            if elem.distance == block22.distance and elem not in record_list:
+                print("block being added when dis == 0:",elem.x,elem.y)
                 block22.setnext(elem)
-
+                main_list.append(elem)
+                record_list.append(elem)
     if not block22.next:
         for elem in tempo_list:
-            if elem.distance - block22.distance == 1 and elem.check == False:
+            if elem.distance - block22.distance == 1 and elem not in record_list:
+                print("block being added when dis == 1:", elem.x, elem.y)
                 block22.setnext(elem)
+                main_list.append(elem)
+                record_list.append(elem)
+    print("after remove:", len(main_list))
+    print("")
+    #for item in record_list:
+        #print(item.x,item.y)
 
-    for block in block22.next:
-        if block.check == False:
-            end = time.time()
-            print("time executed:",end-start)
-            time.sleep(end - start)
-            thread = Process(target=find_path, args=(block.x, block.y, x_goal, y_goal, obstacle,))
+    return main_list
 
-            thread.start()
+def find_path(x_ori, y_ori, x_goal, y_goal, obstacle,main_list):
+    for block in record_list:
+        print("debbug:",block.x,block.y)
+        if block.x == x_goal and block.y == y_goal:
+            print("what's upppppppppp!!!!!!!!!!!!!!!!")
+            return record_list
+    if not main_list:
+        block_list= add_block(x_ori, y_ori, x_goal, y_goal, obstacle,main_list)
+        block_list2 = block_list[:]
+    else:
+        block_list2 = main_list[:]
+   # print("length of main list:",len(main_list))
+    for item in main_list:
+        print("block list 2")
+        add_block(item.x, item.y, x_goal, y_goal, obstacle,block_list2)
+    print("length of block list 2:",len(block_list2))
+    block_list2 = [x for x in block_list2 if x not in main_list]
+    print("length of block list 2 after slice:",len(block_list2))
+    for item in block_list2:
+        return find_path(item.x,item.y,x_goal,y_goal,obstacle,block_list2)
 
 if __name__ == "__main__":
     ob = [Square(4, 4, 0, "block"), Square(4, 4, 1, "block"), Square(4, 4, 2, "block"), Square(4, 4, 3, "block")]
-    find_path(0, 1, 7, 0, ob)
-    for i in main_list:
-        print(i.x, i.y)
+    main_list = []
+    x = find_path(0, 1, 7, 0, ob,main_list)
+    print("length of record list:",len(record_list))
+    for i in x:
+        print("blocks in record:",i.x,i.y)
